95f96b3acba119a8903729a7c2780d4813e23fe4
jsy10101 <jaskiratchawla580@gmail.com>
2024-11-29 03:12:03 -0500

    reset project

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Aora!</Text>
+        </View>
+    );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
