d2bb273b47d6fd89cdfc7de8bb86e833ff06e4e8
jsy10101 <jaskiratchawla580@gmail.com>
2024-11-29 11:55:17 -0500

    create user onboarding/landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,52 @@
-import { Text } from "react-native";
+import { Alert, Image, ScrollView, StatusBar, Text, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 
 const App = () => {
     return (
-        <SafeAreaView className="bg-primary flex-1 items-center justify-center">
-            <Text className="text-3xl font-pblack text-secondary">Aura!</Text>
-            <Link className="text-white font-plight" href="/home">
-                link to home
-            </Link>
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: "100%" }}>
+                <View className="justify-center items-center w-full min-h-[85vh] px-8">
+                    <Image
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode="contain"
+                    />
+                    <Image
+                        source={images.cards}
+                        className="max-w-[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+                    <View className="relative mt-5">
+                        <Text className="text-3xl font-bold text-white text-center">
+                            Discover Endless Opportunities with{" "}
+                            <Text className="text-secondary-200">Aura</Text>
+                        </Text>
+                        <Image
+                            source={images.path}
+                            className="w-[60px] h-[15px] absolute -bottom-2 -right-0"
+                            resizeMode="contain"
+                        />
+                    </View>
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                        Where creativity meets innovation: embark on journey of
+                        limitless exploration with Aura
+                    </Text>
+                    <CustomButton
+                        title="Continue with email"
+                        containerStyles="mt-7 w-full"
+                        handlePress={() =>
+                            Alert.alert(
+                                "email button",
+                                "touchable opacity pressed"
+                            )
+                        }
+                    />
+                </View>
+            </ScrollView>
+            <StatusBar backgroundColor="#161622" barStyle="light-content" />
         </SafeAreaView>
     );
 };
---
+++ components/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+type CustomButtonProps = {
+    title: string;
+    handlePress: () => void;
+    containerStyles: string;
+    textStyles?: string;
+    isLoading?: boolean;
+};
+
+const CustomButton = ({
+    title,
+    handlePress,
+    containerStyles,
+    textStyles = "",
+    isLoading = false,
+}: CustomButtonProps) => {
+    return (
+        <TouchableOpacity
+            className={`bg-secondary min-h-[62px] justify-center items-center rounded-xl ${containerStyles} ${
+                isLoading ? "opacity-50" : ""
+            }`}
+            disabled={isLoading}
+            onPress={handlePress}
+        >
+            <Text
+                className={`text-primary font-psemibold font-lg ${textStyles}`}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default CustomButton;
