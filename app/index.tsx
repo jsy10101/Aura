3e3602775ecabd2932c7735574067cf950f06e7f
jsy10101 <jaskiratchawla580@gmail.com>
2024-11-29 04:40:21 -0500

    set up basic routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Text>Aora!</Text>
-        </View>
+        <SafeAreaView className="bg-primary flex-1 items-center justify-center">
+            <Text className="text-3xl font-pblack text-secondary">Aura!</Text>
+            <Link className="text-white font-plight" href="/home">
+                link to home
+            </Link>
+        </SafeAreaView>
     );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-});
